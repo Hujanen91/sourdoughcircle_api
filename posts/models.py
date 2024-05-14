06e40e5a653f2f371c2ba198bf71dd09b4a6e20f
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2024-05-14 19:58:52 +0000

    Add image size validation in posts serializers

--- posts/models.py
+++ posts/models.py
@@ -7,6 +7,15 @@ class Post(models.Model):
     Post model, related to 'owner', i.e. a User instance.
     Default image set so that we can always reference image.url.
     """
+    image_filter_choices = [
+    ('_1977', '1977'), ('brannan', 'Brannan'),
+    ('earlybird', 'Earlybird'), ('hudson', 'Hudson'),
+    ('inkwell', 'Inkwell'), ('lofi', 'Lo-Fi'),
+    ('kelvin', 'Kelvin'), ('normal', 'Normal'),
+    ('nashville', 'Nashville'), ('rise', 'Rise'),
+    ('toaster', 'Toaster'), ('valencia', 'Valencia'),
+    ('walden', 'Walden'), ('xpro2', 'X-pro II')
+    ]
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -15,7 +24,10 @@ class Post(models.Model):
     image = models.ImageField(
         upload_to='images/', default='../man_kx9guv', blank=True
     )
-
+    image_filter = models.CharField(
+        max_length=32, choices=image_filter_choices, default='normal'
+    )
+    
     class Meta:
         ordering = ['-created_at']
 
