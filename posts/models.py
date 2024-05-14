5c362fe29f12780e2fe3c4cfbaa580beafcc9529
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2024-05-14 19:46:19 +0000

    Edit image in Post model in posts/models.py

--- posts/models.py
+++ posts/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_post_rgq6aq', blank=True
+        upload_to='images/', default='../man_kx9guv', blank=True
     )
 
     class Meta:
