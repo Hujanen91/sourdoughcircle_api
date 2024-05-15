0d31157fd4368ffd703abf13e0afb12c5980bda1
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2024-05-15 10:15:12 +0000

    Add FollowerList in views

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from sourdoughcircle_api.permissions import IsOwnerOrReadOnly
+from followers.models import Follower
+from followers.serializers import FollowerSerializerSerializer
+
+class FollowerList(generics.ListCreateAPIView):
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+        
+
 
 # Create your views here.
