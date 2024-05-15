435dd04bc0f3c70e4c01253384823826ca353ccc
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2024-05-15 20:51:39 +0000

    Add installed apps in settings

--- sourdoughcircle_api/settings.py
+++ sourdoughcircle_api/settings.py
@@ -52,6 +52,11 @@ INSTALLED_APPS = [
     'django_filters',
     'rest_framework.authtoken',
     'dj_rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'dj_rest_auth.registration',
     
     'profiles',
     'posts',
