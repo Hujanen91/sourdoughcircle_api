a2621cc5055e46f53a47fef0a27b4f6e7a58592d
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2024-05-15 10:15:41 +0000

    Add FollowerDetail to views

--- followers/views.py
+++ followers/views.py
@@ -10,7 +10,14 @@ class FollowerList(generics.ListCreateAPIView):
     
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
-        
 
 
-# Create your views here.
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a follower
+    No Update view, as we either follow or unfollow users
+    Destroy a follower, i.e. unfollow someone if owner
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
