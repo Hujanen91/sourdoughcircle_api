061dfb3bc9fb54b4cc4cb2bd610b67d1b60e766e
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2024-05-30 13:11:56 +0000

    Remove email response code due to not working

--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,3 @@
-#from ..sourdoughcircle_api.settings import EMAIL_HOST_USER
 from rest_framework import generics, permissions
 from .models import Contact
 from .serializers import ContactSerializer
@@ -12,28 +11,3 @@ class ContactDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
     permission_classes = [permissions.IsAdminUser]
-    
-    def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serailizer = self.get_serializer(instance, data=request.data)
-        serailizer.is_valid(raise_exception=True)
-        serailizer.save()
-        
-        # Check for updated admin response
-        if request.data.get('admin_response') and not instance.responded:
-            try:
-            # send admin response email back to user email
-                send_email(
-                    subject=request.data.get('Re:' 'subject'),
-                    message=request.data.get('admin_response'),
-                    from_email='EMAIL_HOST_USER',
-                    recipient_list=[instance.email],
-                    fail_silently=True,
-                )
-                instance.responded = True
-                instance.save()
-            except SuspiciousOperation as e:
-                # Handle email sending error
-                print(f"Error sending email: {e}")
-        
-        return Response(serializer.data)
