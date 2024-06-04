adfd4279382a0b535d5a23d664f0f493bf2dcef0
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2024-06-04 20:05:58 +0000

    Add path in urls.py category

--- category/urls.py
+++ category/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from.views import CategoryList, CategoryDetail
+
+router = DefaultRouter()
 
 urlpatterns = [
     path('category/', CategoryList.as_view(), name='category-list'),
-    path('category/<int:pk>/', CategoryDetail.as_view())
+    path('category/<int:pk>/', CategoryDetail.as_view()),
+    path('api/', include(router.urls)),
 ]
