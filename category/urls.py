743b1011c6e51a3912b06862a2e2f6ea3072f5cf
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2024-06-04 09:41:07 +0000

    Edit views

--- category/urls.py
+++ category/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from.views import CategoryList
 
 urlpatterns = [
-    path('category/', views.CategoryList.as_view(), name='category-list'),
+    path('category/', CategoryList.as_view(), name='category-list'),
 ]
