30ba9ef92229d33ccc24a823d0ee3c1664269db6
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2024-06-05 08:52:26 +0000

    Edit category in models.py Posts

--- posts/models.py
+++ posts/models.py
@@ -28,7 +28,7 @@ class Post(models.Model):
     image_filter = models.CharField(
         max_length=32, choices=image_filter_choices, default='normal'
     )
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
     
     class Meta:
         ordering = ['-created_at']
