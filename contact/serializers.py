b7fd9a4586573eb0278a6afd8056f4de2beeaa91
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2024-06-08 21:00:16 +0000

    Hide contact details for non authenticated users

--- contact/serializers.py
+++ contact/serializers.py
@@ -6,3 +6,13 @@ class ContactSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contact
         fields = ['id','name', 'subject', 'email', 'message', 'created_at']
+        
+    def to_representation(self, instance):
+    # Access request object from serializer context
+        request = self.context.get('request')
+        if not request.user.is_authenticated:
+        # Exclude message field for unauthenticated users
+            return { "detail": "Authentication credentials were not provided." }
+        else:
+        # Return all fields for authenticated users
+            return super().to_representation(instance)
