d6d540689ae71c8af25c7d3df0db023c6df115f0
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2024-06-12 07:43:54 +0000

    Edit code in settings.py

--- sourdoughcircle_api/settings.py
+++ sourdoughcircle_api/settings.py
@@ -63,11 +63,11 @@ DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'),
                  'localhost',
-                 '8000-hujanen91-sourdoughcirc-o20yc9srjn6.ws.codeinstitute-ide.net',
+                 '8000-hujanen91-sourdoughcirc-jpubmb1737z.ws-eu114.gitpod.io/',
                  'sourdoughcircle-api-382dc0f20c45.herokuapp.com',
     ]
 
-CSRF_TRUSTED_ORIGINS = ['https://8000-hujanen91-sourdoughcirc-o20yc9srjn6.ws.codeinstitute-ide.net','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://8000-hujanen91-sourdoughcirc-jpubmb1737z.ws-eu114.gitpod.io','https://*.127.0.0.1']
 
 #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 #EMAIL_HOST = 'smtp-mail.outlook.com'
@@ -128,7 +128,7 @@ if 'CLIENT_ORIGIN_DEV' in os.environ:
         r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE
     ).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        rf"{extracted_url}(eu|us)\d+\w\.codeinstitute-ide\.net$",
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
     
 CORS_ALLOW_CREDENTIALS = True
